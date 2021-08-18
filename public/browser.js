89d7be6069cd07e9523c19dbbc76738c47771f83
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-17 21:15:35 -0300

    implemented client-side rendering

--- public/browser.js
+++ public/browser.js
@@ -1,6 +1,22 @@
+const itemsList = document.getElementById('item-list')
+const tasksHTML = items.map( item => {
+  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  <span class="item-text">${item.text}</span>
+  <div>
+    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+  </div>
+</li>`
+}).join('')
+
+console.log(tasksHTML)
+
+itemsList.insertAdjacentHTML('beforeend', tasksHTML)
+
+
 const form = document.querySelector('form[action *= "create-item"]')
 const createInput = document.getElementById('create-field')
-const itemsList = document.getElementById('item-list')
+
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
